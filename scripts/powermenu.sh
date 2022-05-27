4e94136dbf18476c3a464796ea720cbd68f22c07
ozpv <39195175+ozpv@users.noreply.github.com>
2022-05-26 22:19:13 -0500

    Delete powermenu.sh

--- scripts/powermenu.sh
+++
@@ -1,6 +0,0 @@
-case "$(echo -e "Poweroff \nReboot ﰇ\nSuspend ⏾\nCancel ﰸ" | dmenu)" in
-    "Poweroff ") loginctl poweroff ;;
-    "Reboot ﰇ") loginctl reboot ;;
-    "Suspend ⏾") loginctl suspend ;;
-    "Cancel ﰸ") exit ;;
-esac
