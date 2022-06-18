9779914f7a0bdec4837bb2a3612567bc00b9fe6e
ozpv <39195175+ozpv@users.noreply.github.com>
2022-06-17 20:51:07 -0500

    update colors

--- config.h
+++ config.h
@@ -13,11 +13,11 @@ static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#928374";
 static const char col_gray3[]       = "#ebdbb2";
 static const char col_gray4[]       = "#ebdbb2";
-static const char col_cyan[]        = "#928374";
+static const char col_cyan[]        = "#282828";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_cyan },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray2  },
 };
 
 /* tagging */
