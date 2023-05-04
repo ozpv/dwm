2be8ed75d9aa1cddd33cadb74de45856dbbfab43
ozpv <39195175+ozpv@users.noreply.github.com>
2023-05-04 02:32:18 +0000

    update colors and tags

--- config.h
+++ config.h
@@ -16,12 +16,12 @@ static const char col_gray4[]       = "#ebdbb2";
 static const char col_cyan[]        = "#282828";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_cyan },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_gray2  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray3  },
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "" };
+static const char *tags[] = { "/", "www", "", "", };
 
 static const Rule rules[] = {
 	/* xprop(1):
