c6efd49544771588920e14eab44f79f67c51f9f9
ozpv <39195175+ozpv@users.noreply.github.com>
2026-05-20 21:30:10 +0000

    update bars

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@ static const unsigned int gappx     = 12;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 18;        /* 2 is the default spacing around the bar's font */
+static const int user_bh            = 12;        /* 2 is the default spacing around the bar's font */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Regular:size=10" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:style=Regular:size=10";
 static const char col_gray1[]       = "#282828";
@@ -18,8 +18,8 @@ static const char col_gray4[]       = "#ebdbb2";
 static const char col_cyan[]        = "#282828";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_gray3  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray2  },
 };
 
 /* tagging */
